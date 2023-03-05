81a326d51144c116542f9b05b46189c07ac89e32
kuba <jakub.taborowicz@gmail.com>
2023-03-05 07:18:16 +0100

    Commit 7 on development branch.

--- welcome.py
+++ welcome.py
@@ -1,8 +1,10 @@
 # Git in action
+from turtle import color
 
 print('Hello world')
 print('This time is different')
 
 
 class NewCar:
-    pass
+    def __init__(self, color):
+        self.color = color
