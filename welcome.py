a7528ef99539b4734e72623bee7ccee80b9cf700
kuba <jakub.taborowicz@gmail.com>
2023-03-11 15:40:34 +0100

    Added use of kwargs in class initialization.

--- welcome.py
+++ welcome.py
@@ -6,5 +6,6 @@ print('This time is different')
 
 
 class NewCar:
-    def __init__(self, color):
-        self.color = color
+    def __init__(self, colour, *args, **kwargs):
+        self.colour = colour
+        self.__dict__.update(kwargs)
