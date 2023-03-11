80529a0e87d3b112b6eba21a5412efd66285a90b
kuba <jakub.taborowicz@gmail.com>
2023-03-11 15:52:40 +0100

    Added comments and web address of stack overflow tutorial.

--- welcome.py
+++ welcome.py
@@ -4,6 +4,9 @@ from turtle import color
 print('Hello world')
 print('This time is different')
 
+# This example is derived from stack overflow tutorial
+# https://stackoverflow.com/questions/60418497/how-do-i-use-kwargs-in-python-3-class-init-function
+
 
 class NewCar:
     def __init__(self, colour, *args, **kwargs):
